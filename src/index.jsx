17595f4288c22675d119516b779b37d425d832fe
duanliang <duanliang@fengmap.com>
2022-02-26 22:53:35 +0800

    feat: 添加@babel/preset-env预设，允许在任何环境使用最新的js语法特性，在最后打包的时候生成浏览器标准的js; 添加@babel/preset-react，编译react项目;

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import HelloWorld from './components/HelloWorld/index.jsx';
+
+ReactDom.render(<HelloWorld />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,21 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
     },
     devServer: {
         static: './dist',
